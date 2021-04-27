4caae909dd0ed8bd5252c5b750ab514333a166b3
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2021-04-27 22:39:54 +0100

    minor change in free function due to openssl upstream change

--- hpke.c
+++ hpke.c
@@ -2412,7 +2412,7 @@ int hpke_prbuf2evp(
     if (priv) BN_free(priv);
     if (ctx) EVP_PKEY_CTX_free(ctx);
     if (param_bld) OSSL_PARAM_BLD_free(param_bld);
-    if (params) OSSL_PARAM_BLD_free_params(params);
+    if (params) OSSL_PARAM_free(params);
     return(erv);
 err:
 #if defined(SUPERVERBOSE) || defined(TESTVECTORS)
@@ -2421,7 +2421,7 @@ err:
     if (priv) BN_free(priv);
     if (ctx) EVP_PKEY_CTX_free(ctx);
     if (param_bld) OSSL_PARAM_BLD_free(param_bld);
-    if (params) OSSL_PARAM_BLD_free_params(params);
+    if (params) OSSL_PARAM_free(params);
     return(erv);
 }
 
